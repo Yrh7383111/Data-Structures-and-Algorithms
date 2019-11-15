f9542f176802348e0bebb8d451cae31a28f5122f
Jackson Yin <Yrh7383111@outlook.com>
2019-11-14 23:52:16 -0500

    Complete Percolation.java

---
+++ Union_Find/src/percolation/PercolationStats.java
@@ -0,0 +1,43 @@
+package percolation;
+
+
+
+public class PercolationStats
+{
+    // Perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials)
+    {
+
+    }
+
+    // Sample mean of percolation threshold
+    public double mean()
+    {
+
+    }
+
+    // Sample standard deviation of percolation threshold
+    public double stddev()
+    {
+
+    }
+
+    // Low endpoint of 95% confidence interval
+    public double confidenceLo()
+    {
+
+    }
+
+    // High endpoint of 95% confidence interval
+    public double confidenceHi()
+    {
+
+    }
+
+
+    // Test client (see below)
+    public static void main(String[] args)
+    {
+
+    }
+}
