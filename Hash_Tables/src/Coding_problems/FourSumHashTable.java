d8b380ceab7fce9866c281c12cb6e67e84069b4f
Jackson Yin <Yrh7383111@outlook.com>
2020-01-21 11:03:48 -0500

    Add Four Sum (HashTable Version)

---
+++ Hash_Tables/src/Coding_problems/FourSumHashTable.java
@@ -0,0 +1,48 @@
+package Coding_problems;
+
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class FourSumHashTable
+{
+    public static boolean fourSum(int[] a)
+    {
+        // Variables
+        int n = a.length;
+        assert n >= 4;
+
+
+        // Operations
+        HashSet<Integer> s = new HashSet<Integer>();
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                int tmp = a[i] + a[j];
+
+                if (s.contains(tmp))
+                {
+                    System.out.println("Sum is: " + tmp);
+                    System.out.println(i);
+                    System.out.println(j);
+                    return true;
+                }
+                else {
+                    s.add(tmp);
+                }
+            }
+        }
+        return false;
+    }
+
+
+
+    public static void main(String[] args)
+    {
+        int[] nums = new int[]{1, 0, -1, 0, -2, 2};
+
+
+        fourSum(nums);
+    }
+}
