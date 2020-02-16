4b826fce097610089818d24f27a56484f89f7467
Jackson Yin <Yrh7383111@outlook.com>
2020-02-16 15:32:14 -0500

    Add Set

---
+++ ST_Applications/src/Set/WhiteFilter.java
@@ -0,0 +1,33 @@
+package Set;
+
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+
+
+public class WhiteFilter
+{
+    // Do not instantiate.
+    private WhiteFilter() { }
+
+    public static void main(String[] args)
+    {
+        SET<String> set = new SET<String>();
+
+        // read in strings and add to set
+        In in = new In(args[0]);
+        while (!in.isEmpty()) {
+            String word = in.readString();
+            set.add(word);
+        }
+
+        // read in string from standard input, printing out all exceptions
+        while (!StdIn.isEmpty()) {
+            String word = StdIn.readString();
+            if (set.contains(word))
+                StdOut.println(word);
+        }
+    }
+}
